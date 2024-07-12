9102c82354ad59de73aa6e4785acfa1b32d1604a
CollinTheDev <collin.entringer@gmail.com>
2024-07-11 21:18:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('downloadBtn').addEventListener('click', function () {
-    const url = 'https://github.com/CollinTheDev/cat-browser-download/raw/main/teleport_menu.zip';
+    const url = 'https://drive.google.com/uc?export=download&id=1tEyh_xavbEUHxZ6fc9F5WQ_yXS1cL2Uw';
     
     // Create a temporary anchor element and trigger a download
     const a = document.createElement('a');
